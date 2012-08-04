9b17d5691ab6b3c8e774e08b1d0b1d204b115f2f
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-08-04 15:02:26 +0200

    Added class symbol metadata and shift instance method

--- src/fp_oo/core.clj
+++ src/fp_oo/core.clj
@@ -4,20 +4,22 @@
 ;; encapsulated:
 ;;
 ;;     (:x (Point 5 7)) ; -> 5
+;;
+;; However, we can load the resulting hash-map with all metadata we
+;; need (such as: class name).
 (def Point
   (fn [x y]
     {:x x
-     :y y}))
+     :y y
+     :__class_symbol__ 'Point}))
 
 ;; However, for now we can pretend that we already have data-hiding
 ;; and implement some *accessor methods*:
-(def x (fn [this]
-         (:x this)))
+(def x :x)
+(def y :y)
+(def class :__class_symbol__)
 
-(def y (fn [this]
-         (:y this)))
-
-;; A more concise definition would simply be:
-;;
-;;     (def x :x)
-
+(def shift
+  (fn [this xinc yinc]
+    (Point (+ (x this) xinc)
+           (+ (y this) yinc))))
