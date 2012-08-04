7690a14fddf60e9f4884da33e163ac97ff70bf3b
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-08-04 14:39:30 +0200

    Fixed marginalia plugin for lein

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/manuelp/fp-oo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-swank "1.4.4"]]
-  :dev-dependencies [[lein-marginalia "0.7.1"]]
+  :plugins [[lein-swank "1.4.4"]
+            [lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.4.0"]])
