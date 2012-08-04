9af27b8836a3e9e6dbc9b9c8fc905b45cad1f7f3
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-08-04 14:57:54 +0200

    Bare-bones class constructor

--- src/fp_oo/core.clj
+++ src/fp_oo/core.clj
@@ -1,6 +1,23 @@
 (ns fp-oo.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+;; This is a simple constructor, but instance variables are not
+;; encapsulated:
+;;
+;;     (:x (Point 5 7)) ; -> 5
+(def Point
+  (fn [x y]
+    {:x x
+     :y y}))
+
+;; However, for now we can pretend that we already have data-hiding
+;; and implement some *accessor methods*:
+(def x (fn [this]
+         (:x this)))
+
+(def y (fn [this]
+         (:y this)))
+
+;; A more concise definition would simply be:
+;;
+;;     (def x :x)
+
