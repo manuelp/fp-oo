fa7a11483bf9520765bccd83b2bd184d402988ba
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-08-04 14:36:58 +0200

    Modified project-clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject fp-oo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Object system in Clojure following @marick's #fp_oo"
+  :url "https://github.com/manuelp/fp-oo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-swank "1.4.4"]]
+  :dev-dependencies [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.4.0"]])
