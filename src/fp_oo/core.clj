177dc84cf2a73d48ce367a61e9fe3782cc1e49f9
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-08-05 17:14:23 +0200

    Basic inheritance implemented

--- src/fp_oo/core.clj
+++ src/fp_oo/core.clj
@@ -1,41 +1,43 @@
 (ns fp-oo.core)
 
 (defn- class-from-instance
+  "Returns the class from an instance."
   [instance]
   (eval (:__class_symbol__ instance)))
-
-(defn- method-from-message
-  [message class]
-  (message (:__instance_methods__ class)))
-
-;; This class definition exposes an object as a map of instance
-;; methods (or *handlers* for messages that can be sent to instances
-;; of this class).
-;; 
-;; One special method is `add-instance-values`, and is used when
-;; instantiating an object to populate its instance variables (like
-;; Ruby's `initialize` method).
-(def Point
-  {
-   :__own_symbol__ 'Point
-   :__instance_methods__
-   {
-    :add-instance-values (fn [this x y]
-                           (assoc this :x x :y y))
-    :class-name :__class_symbol__
-    :class #(class-from-instance %)
-    :shift (fn [this xinc yinc]
-             (a Point
-                (+ (:x this) xinc)
-                (+ (:y this) yinc)))
-    :add (fn [this other]
-           (send-to this :shift
-                    (send-to other :x) (send-to other :y)))
-    }})
 
 (defn- has-instance-variable?
   [instance variable]
   (not (nil? (variable instance))))
+
+(defn- class-instance-methods
+  "Extract a map with all instance methods of the given class."
+  [class-symbol]
+  (:__instance_methods__ (eval class-symbol)))
+
+(defn- class-symbol-above
+  "Returns the superclass symbol."
+  [class-symbol]
+  (:__superclass_symbol__ (eval class-symbol)))
+
+(defn- lineage
+  "Compiles a seq of symbols for the entire class hierarchy lineage, rooted in `Anything`."
+  [class-symbol]
+  (loop [line (seq [class-symbol])
+         above-sym (class-symbol-above class-symbol)]
+    (if (nil? above-sym)
+      line
+      (recur (cons above-sym line)
+             (class-symbol-above above-sym)))))
+
+;; Next, we can use that function to obtail a seq of maps for all the
+;; instance methods of the class *lineage*. Then we can merge that
+;; maps and obtain all methods available to that class.
+(defn- method-cache
+  [class]
+  (let [class-symbol (:__own_symbol__ class)
+        method-maps (map class-instance-methods
+                         (lineage class-symbol))]
+    (apply merge method-maps)))
 
 ;; Applying a method defined in the class to an instance with some
 ;; arguments can be abstracted by a generic function.
@@ -47,7 +49,7 @@
 ;; "generates" read accessors.
 (defn- apply-message-to
   [class instance message args]
-  (let [method (method-from-message message class)]
+  (let [method (message (method-cache class))]
     (or (and (nil? method)
              (has-instance-variable? instance message)
              (message instance))
@@ -77,3 +79,38 @@
   (fn [instance message & args]
     (let [class (class-from-instance instance)]
       (apply-message-to class instance message args))))
+
+(def Anything
+  {
+   :__own_symbol__ 'Anything
+   :__instance_methods__
+   {
+    ;; Default constructor
+    :add-instance-values identity
+    
+    :class-name :__class_symbol__
+    :class #(class-from-instance %)}})
+
+;; This class definition exposes an object as a map of instance
+;; methods (or *handlers* for messages that can be sent to instances
+;; of this class).
+;; 
+;; One special method is `add-instance-values`, and is used when
+;; instantiating an object to populate its instance variables (like
+;; Ruby's `initialize` method).
+(def Point
+  {
+   :__own_symbol__ 'Point
+   :__superclass_symbol__ 'Anything
+   :__instance_methods__
+   {
+    :add-instance-values (fn [this x y]
+                           (assoc this :x x :y y))
+    :shift (fn [this xinc yinc]
+             (a Point
+                (+ (:x this) xinc)
+                (+ (:y this) yinc)))
+    :add (fn [this other]
+           (send-to this :shift
+                    (send-to other :x) (send-to other :y)))
+    }})
